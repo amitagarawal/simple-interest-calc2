3682fc3947baac57d0e647c0bf19d87a9ca7c9af
amitagarawal <35165776+amitagarawal@users.noreply.github.com>
2021-07-08 00:51:10 +0530

    Added if statement after return

--- SimpleInterestCalc2/src/SimpleInterestCalc.java
+++ SimpleInterestCalc2/src/SimpleInterestCalc.java
@@ -6,7 +6,10 @@ public class SimpleInterestCalc {
 	public double calculateSimpleInterest(int principal, int tenure) {
 		// TODO Auto-generated method stub
 		double amount = principal * (double)tenure / 365 * (rate/100);
-		return amount;		
+		return amount;	
+		
+		if(rate === 0.0){
+		}
 	}
 
 }
